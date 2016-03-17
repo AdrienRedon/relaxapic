609b5b0214f1e169a1c1f0042c8ecc6dba358765
Adrien REDON <AdrienRedon@users.noreply.github.com>
2016-03-17 16:36:19 +0100

    passes and fails and add filter for required and mail

--- app/Core/Validator/Validator.php
+++ app/Core/Validator/Validator.php
@@ -4,5 +4,65 @@ namespace App\Core\Validator;
 
 class Validator 
 {
+    /**
+     * List of available filters
+     * @var array
+     */ 
+    protected $filters = array(
+        'required',
+        'mail'
+    );
+    
+    /**
+     * data to check
+     * @var array
+     */ 
+    protected $data;
+    
+    /**
+     * List of rules
+     * @var array
+     */
+    protected $rules;
+    
+    public function __construct($data = array(), $rules = array()) 
+    {
+        $this->data = $data;
+        $this->rules = $rules;
+    }
+    
+    public function passes()
+    {
+        foreach($this->rules as $field => $rules) {
+            $listRules = explode('|', $rules);
+            foreach($listRules as $rule) {
+                if (strpos($rule, ':') !== false) {
+                    $params = explode(':', $rule);
+                    $filter = array_shift($params);
+                    if (in_array($params[0], $this->filters)) {
+                        $this->$filter($this->data[$field], $params);
+                    }
+                } else {
+                    $this->$rule($this->data[$field]);
+                }
+            }
+        }
+    }
+    
+    public function fails() 
+    {
+        return ! ($this->passes());
+    }
+    
+    /**
+     * Check if a value is a mail
+     * @params $value
+     * @return boolean
+     */ 
+    public function mail($value)
+    {
+        return filter_var($value, FILTER_VALIDATE_EMAIL);
+    }
+    
     
 }
