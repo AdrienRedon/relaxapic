3beb9e2b60809ab1a751f587e8f1cf69af96eeb8
Adrien REDON <redon.adrien@gmail.com>
2016-03-26 19:07:33 +0100

    je merge quand ça marche pas :D

--- public/js/app.js
+++ public/js/app.js
@@ -5,6 +5,7 @@ $('#login__button').on('click', function(e) {
     $('.login__popup').removeClass('hidden');
 });
 
-$('.login__popup').on('click', '.cancel__button', function() {
+$('.login__popup').on('click', '.cancel__button', function(e) {
+    e.preventDefault();
     $(this).parent().addClass('hidden');
 });
