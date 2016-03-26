4365f16583530ad65ad448198f77d82c9652e1d7
Adrien REDON <redon.adrien@gmail.com>
2016-03-26 15:59:51 +0100

    cpe as sql user

--- app/config.php
+++ app/config.php
@@ -3,8 +3,8 @@
 return [
     'sql_host' => '127.0.0.1',
     'sql_base' => 'relaxapic',
-    'sql_login' => 'root',
-    'sql_password' => 'root',
+    'sql_login' => 'cpe',
+    'sql_password' => 'cpe',
 
     'home_route' => ''
 ];
