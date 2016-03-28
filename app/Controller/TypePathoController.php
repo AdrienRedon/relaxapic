85b7bbc19ad0ff565e115cf7ad04fe6106a6960e
Adrien REDON <redon.adrien@gmail.com>
2016-03-28 18:48:15 +0200

    fix some stuff

--- app/Controller/TypePathoController.php
+++ app/Controller/TypePathoController.php
@@ -3,24 +3,12 @@
 namespace App\Controller;
 
 use App\Core\Controller\Controller;
-use App\Libs\Auth;
-use App\Libs\Session;
-use App\Core\DependencyInjection\ContainerInterface;
-use App\Model\Patho;
 
 class TypePathoController extends Controller
 {
-    protected $auth;
-
-    public function __construct(ContainerInterface $container = null)
-    {
-        parent::__construct($container);
-        $this->auth = new Auth($this->container->resolve('SessionInterface'));
-    }
-
     public function getListePatho($idT) 
     {
-        $typePatho = $this->modelResolver->get('TypePatho');
+        $typePatho = $this->model->get('TypePatho');
         $pathos = $typePatho->getListePatho($idT)->toArray();
         $this->view->render('ajax/patho', compact('pathos'));
     }
@@ -31,7 +19,7 @@ class TypePathoController extends Controller
      */
     public function getTypesPatho($idT = null)
     {
-        $typePatho = $this->modelResolver->get('TypePatho');
+        $typePatho = $this->model->get('TypePatho');
         if (isset($idT)) {
             $typesPatho = $typePatho->where(['idT' => $idT])->toArray();
         } else {
